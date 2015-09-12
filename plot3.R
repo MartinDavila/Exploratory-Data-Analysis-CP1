87047b2d3540c56a855c0ca49043a993783ad746
MartinDavila <martin.dahvila@gmail.com>
2015-09-12 11:23:32 -0500

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,55 @@
+#####################################################################################
+## Exploratory Data Analysis, Course Project 1
+## Martín Dâvila
+## 20150912
+#####################################################################################
+## Description of the following script
+## plot3.r
+#####################################################################################
+## This script will perform the following project steps, taken from the web page: 
+## Our overall goal here is simply to examine how household energy usage varies
+## over a 2-day period in February, 2007.
+## Your task is to reconstruct the following plots below,
+## all of which were constructed using the base plotting system.
+#####################################################################################
+
+
+# First of all we need to set the work directory, in which we find all the data sets
+# for me is the following
+setwd("C:/Coursera/Course Project 1 EDA")
+getwd()
+
+
+# Clean up the workspace
+rm(list=ls())
+
+
+
+## Obtaining the dataset in full
+data_full <- read.csv("C:/Coursera/Course Project 1 EDA/household_power_consumption.txt",
+                      header=T, sep=';', na.strings="?", nrows=2075259,
+                      check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+
+## Subsetting the data
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
+
+## Convert the dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Making the Plot 3
+with(data, {
+  plot(Sub_metering_1~Datetime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+
+## Save it to a png file
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
